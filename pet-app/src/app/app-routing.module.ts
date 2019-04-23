251a07fb4a895749f7cf0d7abcc6ab647b75a92a
smaji3789 <subhajit.maji007@gmail.com>
2019-04-23 22:53:21 +1000

    Added petDetails component and data service

--- pet-app/src/app/app-routing.module.ts
+++ pet-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PetDetailsComponent } from './components/pet-details/pet-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/petDetails', pathMatch: 'full' },
+  { path: 'petDetails', component: PetDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pet-app/src/app/app.module.ts
+++ pet-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PetDetailsComponent } from './components/pet-details/pet-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetDetailsComponent
   ],
   imports: [
     BrowserModule,
